731d0eff265afc69f78e1de5c99bcb5a7c3f3ae3
grantkar <hrant87@inbox.ru>
2022-01-30 23:12:50 +0400

    HomeWork 4 added ProductController and setting

--- external-api/src/main/java/ru/gb/external/api/rest/ProductController.java
+++ external-api/src/main/java/ru/gb/external/api/rest/ProductController.java
@@ -1,15 +1,14 @@
 package ru.gb.external.api.rest;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.gb.api.product.api.ProductGateway;
 import ru.gb.api.product.dto.ProductDto;
+import ru.gb.api.productManufacturerDto.ProductManufacturerDto;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,39 +20,35 @@ public class ProductController {
 
     @GetMapping
     public List<ProductDto> getProductList() {
-        return productGateway.findAll();
+        return productGateway.getProductList();
     }
 
     @GetMapping("/info")
     public List<ProductManufacturerDto> getInfoProductList() {
-        return productGateway.findAllInfo();
+        return productGateway.getInfoProductList();
     }
 
     @GetMapping("/{productId}")
     public ResponseEntity<?> getProduct(@PathVariable("productId") Long id) {
-        ProductDto product;
-        if (id != null) {
-            product = productGateway.findById(id);
-            if (product != null) {
-                return new ResponseEntity<>(product, HttpStatus.OK);
-            }
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        ProductDto product;
+//        if (id != null) {
+//            product = productGateway.findById(id);
+//            if (product != null) {
+//                return new ResponseEntity<>(product, HttpStatus.OK);
+//            }
+//        }
+//        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return productGateway.getProduct(id);
     }
 
     @PostMapping
     public ResponseEntity<?> handlePost(@Validated @RequestBody ProductDto productDto) {
-        ProductDto savedProduct = productGateway.save(productDto);
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setLocation(URI.create("/api/v1/product/" + savedProduct.getId()));
-        return new ResponseEntity<>(httpHeaders, HttpStatus.CREATED);
+        return productGateway.handlePost(productDto);
     }
 
     @PutMapping("/{productId}")
     public ResponseEntity<?> handleUpdate(@PathVariable("productId") Long id, @Validated @RequestBody ProductDto productDto) {
-        productDto.setId(id);
-        productGateway.save(productDto);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return productGateway.handleUpdate(id, productDto);
     }
 
     @DeleteMapping("/{productId}")
